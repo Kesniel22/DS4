e155352e63648213657581750a0e572d73da964a
Kesniel <skesniel507@gmail.com>
2024-08-29 00:37:57 -0500

    Laboratorio #2 - Problemas Ejemplos

---
+++ Laboratorio #2/Program2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio__2
+{
+    internal class Program2
+    {
+        public static void Main(string[] args)
+        {
+            //Asignando valor a variable estatica.
+            MyClass.Valor = 1;
+            Console.WriteLine(MyClass.Valor);
+        }
+    }
+    public class MyClass
+    {
+        //Declarando variable estatica
+        public static int Valor;
+    }
+}
