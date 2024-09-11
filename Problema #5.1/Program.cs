163e712dabf8f7b1795d8d9e7944203260cf4eef
Kesniel22 <kesniel.sosa@utp.ac.pa>
2024-09-11 19:26:06 -0400

    Laboratorio #5

---
+++ Problema #5.1/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    private int[] sueldos;
+
+    public void Cargar()
+    {
+        sueldos = new int[6];
+        for (int f = 1; f <= 5; f++)
+        {
+            Console.Write("Ingrese sueldo del operario " + f + ": ");
+            String linea;
+            linea = Console.ReadLine();
+            sueldos[f] = int.Parse(linea);
+        }
+    }
+    public void Imprimir()
+    {
+        Console.Write("Los sueldos de los 5 operarios \n");
+        for (int f = 1;f <= 5; f++)
+        {
+            Console.Write("["+sueldos[f]+"] ");
+        }
+        Console.ReadKey();
+    }
+    private static void Main(string[] args)
+    {
+        Program p = new Program();
+        p.Cargar();
+        p.Imprimir();
+    }
+}
