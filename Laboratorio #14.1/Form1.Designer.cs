0e470d13c2125f61b31d974c71511df6cc4149bb
Kesniel22 <kesniel.sosa@utp.ac.pa>
2024-11-04 16:48:25 -0500

    Lab#14 - Completado

---
+++ Laboratorio #14.1/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace Laboratorio__14._1
+{
+    partial class frmProductos
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmProductos));
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.txtStock = new System.Windows.Forms.TextBox();
+            this.tsbNuevo = new System.Windows.Forms.Button();
+            this.tsbGuardar = new System.Windows.Forms.Button();
+            this.tsbCancelar = new System.Windows.Forms.Button();
+            this.tsbEliminar = new System.Windows.Forms.Button();
+            this.tsbBuscar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tstId = new System.Windows.Forms.TextBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.SuspendLayout();
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(17, 88);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(75, 20);
+            this.txtId.TabIndex = 0;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(152, 88);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(475, 20);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(17, 179);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(100, 20);
+            this.txtPrecio.TabIndex = 2;
+            // 
+            // txtStock
+            // 
+            this.txtStock.Location = new System.Drawing.Point(152, 179);
+            this.txtStock.Name = "txtStock";
+            this.txtStock.Size = new System.Drawing.Size(146, 20);
+            this.txtStock.TabIndex = 3;
+            // 
+            // tsbNuevo
+            // 
+            this.tsbNuevo.ImageKey = "nuevo.png";
+            this.tsbNuevo.ImageList = this.imageList1;
+            this.tsbNuevo.Location = new System.Drawing.Point(14, 12);
+            this.tsbNuevo.Name = "tsbNuevo";
+            this.tsbNuevo.Size = new System.Drawing.Size(32, 31);
+            this.tsbNuevo.TabIndex = 4;
+            this.tsbNuevo.UseVisualStyleBackColor = true;
+            this.tsbNuevo.Click += new System.EventHandler(this.tsbNuevo_Click);
+            // 
+            // tsbGuardar
+            // 
+            this.tsbGuardar.ImageKey = "guardar.png";
+            this.tsbGuardar.ImageList = this.imageList1;
+            this.tsbGuardar.Location = new System.Drawing.Point(52, 12);
+            this.tsbGuardar.Name = "tsbGuardar";
+            this.tsbGuardar.Size = new System.Drawing.Size(32, 31);
+            this.tsbGuardar.TabIndex = 5;
+            this.tsbGuardar.UseVisualStyleBackColor = true;
+            this.tsbGuardar.Click += new System.EventHandler(this.tsbGuardar_Click);
+            // 
+            // tsbCancelar
+            // 
+            this.tsbCancelar.ImageKey = "cancelar.png";
+            this.tsbCancelar.ImageList = this.imageList1;
+            this.tsbCancelar.Location = new System.Drawing.Point(90, 12);
+            this.tsbCancelar.Name = "tsbCancelar";
+            this.tsbCancelar.Size = new System.Drawing.Size(32, 31);
+            this.tsbCancelar.TabIndex = 6;
+            this.tsbCancelar.UseVisualStyleBackColor = true;
+            this.tsbCancelar.Click += new System.EventHandler(this.tsbCancelar_Click);
+            // 
+            // tsbEliminar
+            // 
+            this.tsbEliminar.ImageKey = "eliminar.png";
+            this.tsbEliminar.ImageList = this.imageList1;
+            this.tsbEliminar.Location = new System.Drawing.Point(128, 12);
+            this.tsbEliminar.Name = "tsbEliminar";
+            this.tsbEliminar.Size = new System.Drawing.Size(32, 31);
+            this.tsbEliminar.TabIndex = 7;
+            this.tsbEliminar.UseVisualStyleBackColor = true;
+            this.tsbEliminar.Click += new System.EventHandler(this.tsbEliminar_Click);
+            // 
+            // tsbBuscar
+            // 
+            this.tsbBuscar.ImageKey = "buscar.png";
+            this.tsbBuscar.ImageList = this.imageList1;
+            this.tsbBuscar.Location = new System.Drawing.Point(452, 12);
+            this.tsbBuscar.Name = "tsbBuscar";
+            this.tsbBuscar.Size = new System.Drawing.Size(32, 31);
+            this.tsbBuscar.TabIndex = 8;
+            this.tsbBuscar.UseVisualStyleBackColor = true;
+            this.tsbBuscar.Click += new System.EventHandler(this.tsbBuscar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Location = new System.Drawing.Point(17, 235);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(100, 32);
+            this.btnSalir.TabIndex = 9;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Franklin Gothic Medium", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(167, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 17);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Buscar por ID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Franklin Gothic Medium", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(14, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 17);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "ID:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Franklin Gothic Medium", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(149, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 17);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Nombre:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Franklin Gothic Medium", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(14, 159);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 17);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Precio:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Franklin Gothic Medium", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(149, 159);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 17);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Stock:";
+            // 
+            // tstId
+            // 
+            this.tstId.Location = new System.Drawing.Point(258, 18);
+            this.tstId.Name = "tstId";
+            this.tstId.Size = new System.Drawing.Size(188, 20);
+            this.tstId.TabIndex = 15;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "buscar.png");
+            this.imageList1.Images.SetKeyName(1, "cancelar.png");
+            this.imageList1.Images.SetKeyName(2, "eliminar.png");
+            this.imageList1.Images.SetKeyName(3, "guardar.png");
+            this.imageList1.Images.SetKeyName(4, "nuevo.png");
+            // 
+            // frmProductos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(646, 282);
+            this.Controls.Add(this.tstId);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.tsbBuscar);
+            this.Controls.Add(this.tsbEliminar);
+            this.Controls.Add(this.tsbCancelar);
+            this.Controls.Add(this.tsbGuardar);
+            this.Controls.Add(this.tsbNuevo);
+            this.Controls.Add(this.txtStock);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtId);
+            this.Name = "frmProductos";
+            this.Text = "frmProductos";
+            this.Load += new System.EventHandler(this.frmProductos_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.TextBox txtStock;
+        private System.Windows.Forms.Button tsbNuevo;
+        private System.Windows.Forms.Button tsbGuardar;
+        private System.Windows.Forms.Button tsbCancelar;
+        private System.Windows.Forms.Button tsbEliminar;
+        private System.Windows.Forms.Button tsbBuscar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tstId;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
+
